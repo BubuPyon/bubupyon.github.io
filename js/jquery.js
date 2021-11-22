078800cf51f51a3e84797b2883394169a5dc590c
BubuPyon <pyonbubu@gmail.com>
2021-11-22 12:03:55 +0700

    first commit

---
+++ js/jquery.js
@@ -0,0 +1,47 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    // nav:true,
+    // responsive:{
+    //     0:{
+    //         items:1
+    //     },
+    //     600:{
+    //         items:2
+    //     },
+    //     1000:{
+    //         items:3
+    //     }
+    // }
+    autoplay:true,
+    // autoplayTimeout:1000,
+    // autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+// $('.owl-carousel, #owl-carousel-2').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:1
+//         },
+//         1000:{
+//             items:1
+//         }
+//     }
+// })
